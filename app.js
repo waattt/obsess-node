350d6af9a13214e1dab6cd1b55bc303b28ce6505
waattt <timonvandreumel@gmail.com>
2014-05-17 01:12:27 +0200

    Lock ledPins to specific connection
    TODO: Does not work properly on mobile; the socket does not disconnect
    when the screen is turned of or if the user switches to another app.
    SOLUTION: https://github.com/mathiasbynens/jquery-visibility (this still
    has to be implemented(

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var app = require('express.io')(),
     fs = require('fs'),
     firmata = require('/usr/local/lib/node_modules/firmata'),
     board = new firmata.Board('/dev/ttyACM0', arduinoReady);
-
-var ledPin = 13;
  
 function arduinoReady(err) {
     if (err) {
@@ -16,26 +14,15 @@ function arduinoReady(err) {
       + '.' + board.firmware.version.minor);
  
     var ledOn = true;
-    board.pinMode(ledPin, board.MODES.OUTPUT);
+	for (var i = 0; i < ledPins; i++) {
+		board.pinMode(ledPins[i], board.MODES.OUTPUT);
+	}
 }
  
 app.http().io();
 app.listen(8080);
 console.log("Listening on http://192.168.2.9:8080...");
- 
-/* directs page requests to html files
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
- 
-    res.writeHead(200);
-    res.end(data);
-  });
-}*/
+
 // a convenient variable to refer to the HTML directory
 var html_dir = './static/';
 
@@ -50,30 +37,62 @@ app.get('/hello', function(req, res) {
 });*/
  
 // this handles socket.io comm from html files
- 
+ // de arrays zijn globaal
+var ledPins = [
+	13,
+	12,
+	11
+];
+var ledPinsBezet = new Array();
 app.io.sockets.on('connection', function(socket) {
     socket.send('connected...');
- 
-    socket.on('message', function(data) {
-        if (data == 'turn on') {
-            console.log('+');
-            board.digitalWrite(ledPin, board.HIGH);
-            socket.broadcast.send("let there be light!");
-        }
-        if (data == 'turn off') {
-            console.log('-');
-            board.digitalWrite(ledPin, board.LOW);
-            socket.broadcast.send("who turned out the light?");
-        }
-        return;
-    });
+    socket.send('Bezig met ledPin toewijzen...');
+	
+	var ledPin;
+	console.log('ledPins.length = ' + ledPins.length);
+	if(ledPins.length > 0){
+		var maakBezet = ledPins.shift();
+		
+		ledPin = maakBezet;
+		socket.send('Toegewezen ledPin is ' + ledPin);
+		
+		ledPinsBezet.unshift(maakBezet);
+		console.log('Maak bezet: '+ maakBezet);
+		console.log('Led Pins Bezet: ' + ledPinsBezet);
+				
+		socket.on('message', function(data) {
+			if (data == 'turn on') {
+				console.log('+');
+				board.digitalWrite(ledPin, board.HIGH);
+				socket.broadcast.send("let there be light!");
+			}
+			if (data == 'turn off') {
+				console.log('-');
+				board.digitalWrite(ledPin, board.LOW);
+				socket.broadcast.send("who turned out the light?");
+			}
+			return;
+		});
+	}else {
+		socket.send('Sorry alle pins zijn bezet.');
+		socket.emit('BOEM! Hij zit vol. Aantal gebruikers verbonden:' + ledPinsBezet.lenght);
+	}
  
     socket.on('disconnect', function() {
         socket.send('disconnected...');
+		console.log('ledPinsBezet.length = ' + ledPinsBezet.length);
+		if(ledPinsBezet.length > 0){
+			var maakVrij = ledPinsBezet.indexOf(ledPin);
+			if(maakVrij != -1){
+				maakVrij = ledPinsBezet.splice(maakVrij, 1);
+				ledPins.unshift(maakVrij);
+				console.log('Maakt Vrij: ' + maakVrij);
+				console.log('Led Pins: ' + ledPins);
+			}
+		}
     });
 });
 var socketCount = 0;
- 
 app.io.sockets.on('connection', function(socket){
     // Socket has connected, increase socket count
     socketCount++;
