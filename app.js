53635f8f98577560498cc09b687399f90a764cc0
Timon van Dreumel <timonvandreumel@gmail.com>
2014-06-01 12:22:28 +0000

    Motor animation and calibration updates. Also added led flash instead of toggle

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-var app = require('express.io')(), 
-	express = require('express.io'),
-    fs = require('fs'),
-    firmata = require('/usr/local/lib/node_modules/firmata'),
-    board = new firmata.Board('/dev/ttyACM0', arduinoReady);
- 
 var ledPins = [
 	6,
 	5,
@@ -20,6 +14,18 @@ var enablePin = 11;
 var motorPin1 = 10;
 var motorPin2 = 9;
 
+//initialize express / socket.io / firmata(arduino firmware)
+var app = require('express.io')(), 
+	express = require('express.io'),
+    fs = require('fs'),
+    firmata = require('/usr/local/lib/node_modules/firmata'),
+    board = new firmata.Board('/dev/ttyACM0', arduinoReady);
+
+app.http().io();
+app.listen(8080);
+console.log("Listening on port 8080");
+
+// Arduino Ready
 function arduinoReady(err) {
     if (err) {
         console.log(err);
@@ -28,19 +34,117 @@ function arduinoReady(err) {
     console.log('Arduino connected. Firmware: ' + board.firmware.name 
       + '-' + board.firmware.version.major 
       + '.' + board.firmware.version.minor);
- 
+	
+	
 	for (var i = 0; i < ledPins; i++) {
 		board.pinMode(ledPins[i], board.MODES.OUTPUT);
 	}
 	
-	board.pinMode(enablePin, board.MODES.OUTPUT);
+	board.pinMode(topSensor, board.MODES.INPUT);
+	board.pinMode(botSensor, board.MODES.INPUT);
+	
+		//test sensors
+		//board.digitalRead(topSensor,function(value){console.log('Test topsensor: ' + value);});
+		//board.digitalRead(botSensor,function(value){console.log('Test botsensor: ' + value);});
+	
+	board.pinMode(enablePin, board.MODES.PWM);
 	board.pinMode(motorPin1, board.MODES.OUTPUT);
 	board.pinMode(motorPin2, board.MODES.OUTPUT);
+    
+		//test motor - omlaag
+		/*board.analogWrite(enablePin, 255);
+		board.digitalWrite(motorPin1, board.LOW);
+		board.digitalWrite(motorPin2, board.HIGH);*/
+	
+	//  MOTOR IZZL (op en neer)
+	var motorMode = 0;
+	
+	var loopje;
+	var loopje_count = 0;
+	motorModes(motorMode);
+	function motorModes(mode){
+		
+		if(loopje_count >= 1){
+			clearInterval(loopje);
+			loopje_count = 0;
+		}
+		
+		//this part puts the motor in the right place - all the way down.
+		setMotor(255, 1); 
+		var timeout = setTimeout(function(){	
+			setMotor(255, 0); 
+			board.digitalRead(botSensor,function(value){
+				if (value === 1) {
+					setMotor(0, 1);
+					animateMotor();
+				}
+			});
+		}, 200);
+		
+		function animateMotor(){
+			//here come the modes
+			if(motorMode == 0){		
+				var delay = 1000;
+				var interval = 150 + delay;
+				
+				if(loopje_count == 0){
+					var up = false;
+					loopje = setInterval(function(){
+						if(up == false){
+							setMotor(0, 1);
+							setTimeout(function(){	
+								setMotor(200, 1);
+								console.log('omhoog');
+							},delay);
+						}
+						if(up == true){
+							setMotor(0, 1);
+							setTimeout(function(){	
+								setMotor(70, 0);
+								console.log('omlaag');
+							},delay);
+						}
+						up = !up;
+					}, interval);
+					loopje_count++;
+				}
+			}
+			/*if(mode == 1){
+				setMotor(200, 1);
+				setTimeout(function(){	
+					setMotor()
+				}
+			}*/
+		}
+	}
+	
+	// making sure it goes the other way when reaching the end
+	board.digitalRead(topSensor,function(value){
+		if(value == 1){
+			motorModes(motorMode);
+		}
+	});
+	/*
+	board.digitalRead(botSensor,function(value){
+		if(value == 1){
+			motorModes(motorMode);
+		}
+	});*/
+	
+	//not having to write this every time
+	function setMotor(speed, reverse){
+		if(reverse == 0){
+			board.analogWrite(enablePin, speed);
+			board.digitalWrite(motorPin1, board.LOW);
+			board.digitalWrite(motorPin2, board.HIGH);
+		}
+		if(reverse == 1){
+			board.analogWrite(enablePin, speed);
+			board.digitalWrite(motorPin1, board.HIGH);
+			board.digitalWrite(motorPin2, board.LOW);
+		}
+	}	
 }
- 
-app.http().io();
-app.listen(8080);
-console.log("Listening on http://192.168.2.9:8080...");
 
 // a convenient variable to refer to the static directory
 var html_dir = './static/';
@@ -50,44 +154,10 @@ app.use(express.static(__dirname + '/static'));
 app.get('/', function(req, res) {
     res.sendfile(html_dir + 'index.html');
 });
-/* Note: route names need not match the file name
-app.get('/hello', function(req, res) {
-    res.sendfile(html_dir + 'hello.html');
+/* NOTE: Nog een status pagina maken, zodat je 'als admin' kan zien hoe het ervoor staat.
+app.get('/status', function(req, res) {
+    res.sendfile(html_dir + 'status.html');
 });*/
-/* //////////////////////////////////////////////////////
-////////////////// BEGIN DIKKE SJIT /////////////////////
-///////////////////////////////////////////////////////*/
-
-//  MOTOR IZZL (op en neer)
-var speed = 255;
-var reverse = 0;
-board.digitalRead(topSensor,function(value){
-	if (value === 1) {
-		setMotor(255, 0);
-		console.log('motor reached top');
-	}
-});
-board.digitalRead(botSensor,function(value){
-	if (value === 1) {
-		setMotor(255, 1);
-		console.log('motor reached bot');
-	}
-});
-
-setMotor(speed, reverse);
-
-function setMotor(speed, reverse){
-	if(reverse == 0){
-		board.analogWrite(enablePin, speed);
-		board.digitalWrite(motorPin1, board.LOW);
-		board.digitalWrite(motorPin2, board.HIGH);
-	}
-	if(reverse == 1){
-		board.analogWrite(enablePin, speed);
-		board.digitalWrite(motorPin1, board.HIGH);
-		board.digitalWrite(motorPin2, board.LOW);
-	}
-}
 
 // this handles socket.io comm from html files
 
@@ -97,7 +167,6 @@ app.io.sockets.on('connection', function(socket) {
     socket.send('Bezig met ledPin toewijzen...');
 	
 	var ledPin;
-	console.log('ledPins.length = ' + ledPins.length);
 	if(ledPins.length > 0){
 		var maakBezet = ledPins.shift();
 		
@@ -107,11 +176,13 @@ app.io.sockets.on('connection', function(socket) {
 		ledPinsBezet.unshift(maakBezet);
 				
 		socket.on('message', function(data) {
-			if (data == 'turn on') {
+			if (data == 'clicked') {
 				board.digitalWrite(ledPin, board.HIGH);
-			}
-			if (data == 'turn off') {
-				board.digitalWrite(ledPin, board.LOW);
+				clearTimeout(flash); //cancel out the other delay if repeaditly pressed
+				var delay=200;// ms
+				var flash = setTimeout(function(){
+					board.digitalWrite(ledPin, board.LOW);
+				},delay); 
 			}
 			return;
 		});
